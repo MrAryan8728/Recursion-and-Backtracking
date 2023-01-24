6144dddcfb792f702d868c46f3a5b2590f556d84
Tiwari ji <tiwariarya8728@gmail.com>
2023-01-24 18:29:03 +0530

    All done

--- Intro.cpp
+++ Intro.cpp
@@ -8,17 +8,51 @@
 using namespace std;
 
 // 1. print name 5 times.
-void print(string s,int n)
+/*void print(string s,int n)
 {
     if(n == 5)
     return;
     cout<<s<<", "<<"\n";
     n++;
     print(s,n);
+}*/
+
+//2.print from 1 to n
+void print_1toN(int n,int j)
+{
+    if(j > n)
+    return;
+    cout<<j<<" ";
+    print_1toN(n,j+1);
+}
+
+// 3. print linearly from n to 1
+void print_linear(int n)
+{
+    if(n < 1)
+    return;
+    cout<<n<<" ";
+    n--;
+    print_linear(n);
+}
+
+//4.print from n to 1 using Backtracking.
+void print_1toN_B(int i,int j)
+{
+    if(i > j)
+    return;
+    print_1toN_B(i + 1,j);
+    cout<<i<<" ";   
 }
 
 int main() {
-    string s;
-    cin>>s;
-    print(s,0);
+    // string s;
+    // cin>>s;
+    // print(s,0);
+
+    int n;
+    cin>>n;
+    // print_linear(n);
+    // print_1toN(n,1);
+    print_1toN_B(1,n);
 }
